cd098fed95eb65feedd2ae7e71eb973cbaed62d3
Spencer Rauch <rauchs@gmail.com>
2023-08-30 06:33:19 -0700

    mornin algo day 3

--- algos/SinglyLinkedLists.js
+++ algos/SinglyLinkedLists.js
@@ -152,7 +152,40 @@ class SinglyLinkedList {
     let prevHead = this.head;
     this.head = prevHead.next;
     return prevHead.data;
+  }
+
+  /**
+  * Removes the last node of this list.
+  * - Time: O(?).
+  * - Space: O(?).
+  * @returns {any} The data from the node that was removed.
+  */
+  removeBack() {
     //Your code here
+   }
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The data to search for in the nodes of this list.
+   * @returns {boolean}
+   */
+  contains(val) { 
+    //Your code here
+  }
+
+  /**
+   * Determines whether or not the given search value exists in this list.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {any} val The data to search for in the nodes of this list.
+   * @param {?ListNode} current The current node during the traversal of this list
+   *    or null when the end of the list has been reached.
+   * @returns {boolean}
+   */
+  containsRecursive(val, current = this.head) { 
+    //Your code ehre
   }
 
 }
