4a2b726747e7d11d2a5308bdda98322a4bdae596
Spencer Rauch <rauchs@gmail.com>
2023-09-08 07:07:01 -0700

    Friday algo

--- algos/BinarySearchTrees.js
+++ algos/BinarySearchTrees.js
@@ -266,44 +266,93 @@ class BinarySearchTree {
         return 1 + this.size(node.left) + this.size(node.right);
     }
 
+    /*
+    If you know you need to explore the roots before inspecting any leaves,
+    you pick pre-order because you will encounter all the roots before all of the leaves.
+    
+    If you know you need to explore all the leaves before any nodes, 
+    you select post-order because you don't waste any time inspecting roots in search for leaves.
+    
+    If you know that the tree has an inherent sequence in the nodes,
+    and you want to flatten the tree back into its original sequence, than an in-order traversal should be used. 
+    The tree would be flattened in the same way it was created. 
+    */
 
+    /**
+     * DFS Inorder: (Left, CurrNode, Right)
+     * Converts this BST into an array following Depth First Search inorder.
+     * See debugger call stack to help understand the recursion.
+     * Example on the fullTree var:
+     * [4, 10, 12, 15, 18, 22, 24, 25, 31, 35, 44, 50, 66, 70, 90]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrInorder(node = this.root, vals = []) { 
+        //Your code here
+    }
+
+    /**
+     * DFS Preorder: (CurrNode, Left, Right)
+     * Converts this BST into an array following Depth First Search preorder.
+     * Example on the fullTree var:
+     * [25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrPreorder(node = this.root, vals = []) { 
+        //Your code here
+    }
+
+
+    /**
+     * DFS Postorder (Left, Right, CurrNode)
+     * Converts this BST into an array following Depth First Search postorder.
+     * Example on the fullTree var:
+     * [4, 12, 10, 18, 24, 22, 15, 31, 44, 35, 66, 90, 70, 50, 25]
+     * @param {Node} node The current node during the traversal of this tree.
+     * @param {Array<number>} vals The data that has been visited so far.
+     * @returns {Array<number>} The vals in DFS Preorder once all nodes visited.
+     */
+    toArrPostorder(node = this.root, vals = []) { 
+        //Your code here
+    }
 }
 
-const emptyTree = new BinarySearchTree();
-const oneNodeTree = new BinarySearchTree();
-oneNodeTree.root = new BSTNode(10);
-
-/* twoLevelTree
-        root
-        10
-      /   \
-    5     15
+/* fullTree
+                    root
+                <-- 25 -->
+              /            \
+            15             50
+          /    \         /    \
+        10     22      35     70
+      /   \   /  \    /  \   /  \
+    4    12  18  24  31  44 66  90
 */
-const twoLevelTree = new BinarySearchTree();
-twoLevelTree.root = new BSTNode(10);
-twoLevelTree.root.left = new BSTNode(5);
-twoLevelTree.root.right = new BSTNode(15);
+const fullTree = new BinarySearchTree();
+fullTree
+    .insert(25)
+    .insert(15)
+    .insert(10)
+    .insert(22)
+    .insert(4)
+    .insert(12)
+    .insert(18)
+    .insert(24)
+    .insert(50)
+    .insert(35)
+    .insert(70)
+    .insert(31)
+    .insert(44)
+    .insert(66)
+    .insert(90);
 
-/* threeLevelTree 
-        root
-        10
-      /   \
-    5     15
-  / \    / \
-2   6  13  
-*/
-const threeLevelTree = new BinarySearchTree();
-threeLevelTree.root = new BSTNode(10);
-threeLevelTree.root.left = new BSTNode(5);
-threeLevelTree.root.left.left = new BSTNode(2);
-threeLevelTree.root.left.right = new BSTNode(6);
-threeLevelTree.root.right = new BSTNode(15);
-threeLevelTree.root.right.left = new BSTNode(13);
+// in order 
+//console.log(fullTree.toArrInorder())
 
-console.log("*****************************");
-// emptyTree.print();
-console.log("*****************************");
-// twoLevelTree.print();
-console.log("*****************************");
-// threeLevelTree.insertRecursive(12);
-// threeLevelTree.print();
+//pre order
+//console.log(fullTree.toArrPreorder())
+
+//post order
+//console.log(fullTree.toArrPostorder())
